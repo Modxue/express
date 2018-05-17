c466065987349f04861af28f5a79c4cc809dc08c
GreyYa <2667579717@qq.com>
2018-05-17 11:18:23 +0800

    代码更新

--- express-dao/src/main/java/edu/zut/cs/express/admin/dao/User_usedao.java
+++ express-dao/src/main/java/edu/zut/cs/express/admin/dao/User_usedao.java
@@ -1,10 +1,17 @@
 package edu.zut.cs.express.admin.dao;
 
+import java.util.List;
+
 import edu.zut.cs.express.admin.domain.User_use;
 import edu.zut.cs.express.base.dao.GenericDao;
 
 public interface User_usedao extends GenericDao<User_use, Long> {
 	
+	public static String findAll(String name){
+		
+		
+		return name;
+	}
 	
 	
 	
